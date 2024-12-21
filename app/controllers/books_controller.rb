bbc2e0596e2de998113e7453ee5f536c34ee9633
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2024-12-21 09:51:57 +0000

    [Update] bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,23 +24,31 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.newz1
+    @book = Book.new
+    @users = @users = User.all
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.all
   end
 
   def edit
     @book = Book.find(params[:id])
     @books = Book.all
+    # if @book.update(book_params)
+    #   flash[:notice] = "You have updated book successfully."
+    #   redirect_to book_path(@book.id)
+    # else
+    #   render :edit_book
+    # end
   end
 
     # 投稿データのストロングパラメータ
     private
 
     def book_params
-      params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :body)   if params[:book].present?
     end
     
 end
